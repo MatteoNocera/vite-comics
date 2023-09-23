e1f0b772354374e5042d0f6b315d5c916b98f6eb
MatteoNocera <matteo_nocera@tiscali.it>
2023-09-23 15:46:25 +0200

    insert dinamically link on AppBuy

--- src/components/AppBuy.vue
+++ src/components/AppBuy.vue
@@ -4,13 +4,13 @@
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5 justify-content-between justify-content-md-center">
 
 
-                <div class="col1">
+                <div class="col1" v-for="action in actions">
                     <a href="#">
-                        <img src="../assets/img/buy-comics-digital-comics.png" alt=""><span>DIGITAL COMICS</span>
+                        <img :src="action.img" alt=""><span>{{ action.text }}</span>
                     </a>
                 </div>
 
-                <div class="col1">
+                <!-- <div class="col1">
                     <a href="#">
                         <img src="../assets/img/buy-comics-merchandise.png" alt=""><span>DC MERCHANDISE</span>
                     </a>
@@ -32,7 +32,7 @@
                     <a href="#">
                         <img src="../assets/img/buy-dc-power-visa.svg" alt=""><span>DC POWER VISA</span>
                     </a>
-                </div>
+                </div> -->
 
             </div>
 
@@ -44,6 +44,37 @@
 
 <script>
 export default {
-    name: 'AppBuy'
+    name: 'AppBuy',
+    data() {
+        return {
+            actions: [
+                {
+                    img: 'src/assets/img/buy-comics-digital-comics.png',
+                    text: 'DIGITAL COMICS'
+
+                },
+                {
+                    img: 'src/assets/img/buy-comics-merchandise.png',
+                    text: 'DC MERCHANDISE'
+
+                },
+                {
+                    img: 'src/assets/img/buy-comics-subscriptions.png',
+                    text: 'SUBSCRIPTION'
+
+                },
+                {
+                    img: 'src/assets/img/buy-comics-shop-locator.png',
+                    text: 'COMIC SHOP LOCATOR'
+
+                },
+                {
+                    img: 'src/assets/img/buy-dc-power-visa.svg',
+                    text: 'DC POWER VISA'
+
+                },
+            ]
+        }
+    }
 }
 </script>
