6c8bad7d7238ce115724d02f9babcf803a0814c8
MatteoNocera <matteo_nocera@tiscali.it>
2023-09-25 13:43:11 +0200

    add AppCard

---
+++ src/components/AppCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card">
+        <img :src="thumb" alt="">
+        <div class="card-body">
+            <h4 class="card-text">{{ series }}</h4>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'AppCard',
+
+    props: {
+        thumb: String,
+        price: String,
+        series: String,
+        type: String
+    }
+
+}
+</script>
+
+<style lang="scss" scoped></style>
