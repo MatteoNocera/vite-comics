cf05eb56a761093455f2c76ecd247bd57c81865c
MatteoNocera <matteo_nocera@tiscali.it>
2023-09-25 11:42:13 +0200

    insert method to import img

--- src/components/AppBuy.vue
+++ src/components/AppBuy.vue
@@ -3,36 +3,11 @@
         <div class="container py-5">
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5 justify-content-between justify-content-md-center">
 
-
                 <div class="col1" v-for="action in actions">
                     <a href="#">
-                        <img :src="action.img" alt=""><span>{{ action.text }}</span>
+                        <img :src="getImageUrl(action.img)" alt=""><span>{{ action.text }}</span>
                     </a>
                 </div>
-
-                <!-- <div class="col1">
-                    <a href="#">
-                        <img src="../assets/img/buy-comics-merchandise.png" alt=""><span>DC MERCHANDISE</span>
-                    </a>
-                </div>
-
-                <div class="col1">
-                    <a href="#">
-                        <img src="../assets/img/buy-comics-subscriptions.png" alt=""><span>SUBSCRIPTION</span>
-                    </a>
-                </div>
-
-                <div class="col1">
-                    <a href="#">
-                        <img src="../assets/img/buy-comics-shop-locator.png" alt=""><span>COMIC SHOP LOCATOR</span>
-                    </a>
-                </div>
-
-                <div class="col1">
-                    <a href="#">
-                        <img src="../assets/img/buy-dc-power-visa.svg" alt=""><span>DC POWER VISA</span>
-                    </a>
-                </div> -->
 
             </div>
 
@@ -49,31 +24,36 @@ export default {
         return {
             actions: [
                 {
-                    img: 'src/assets/img/buy-comics-digital-comics.png',
+                    img: '../assets/img/buy-comics-digital-comics.png',
                     text: 'DIGITAL COMICS'
 
                 },
                 {
-                    img: 'src/assets/img/buy-comics-merchandise.png',
+                    img: '../assets/img/buy-comics-merchandise.png',
                     text: 'DC MERCHANDISE'
 
                 },
                 {
-                    img: 'src/assets/img/buy-comics-subscriptions.png',
+                    img: '../assets/img/buy-comics-subscriptions.png',
                     text: 'SUBSCRIPTION'
 
                 },
                 {
-                    img: 'src/assets/img/buy-comics-shop-locator.png',
+                    img: '../assets/img/buy-comics-shop-locator.png',
                     text: 'COMIC SHOP LOCATOR'
 
                 },
                 {
-                    img: 'src/assets/img/buy-dc-power-visa.svg',
+                    img: '../assets/img/buy-dc-power-visa.svg',
                     text: 'DC POWER VISA'
 
                 },
             ]
+        }
+    },
+    methods: {
+        getImageUrl: function (img) {
+            return new URL(img, import.meta.url)
         }
     }
 }
